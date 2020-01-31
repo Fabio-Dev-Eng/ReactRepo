b6821400cd488635fb8aff87d1c4a21928994cc7
Fabio-Dev-Eng <60468796+Fabio-Dev-Eng@users.noreply.github.com>
2020-01-31 16:46:52 +0100

    F
    Commit Filter PaidFines

--- src/App.js
+++ src/App.js
@@ -5,100 +5,314 @@ import "./styles.css";
 
 const MOCK_FINES = [
   {
-    id: 1,
+    id: 701,
     reason: "Gareggiamento in velocità",
-    plate: "AB123",
-    paid: true
-  },
-  {
-    id: 2,
-    reason: "Eccesso velocità",
-    plate: "AB123",
-    paid: true
-  },
-  {
-    id: 3,
-    reason: "Passaggio con il rosso",
-    plate: "AB123",
+    plate: "EF 888 TY",
     paid: false
   },
   {
-    id: 4,
-    reason: "Passaggio con il rosso",
-    plate: "AB123",
+    id: 702,
+    reason: "Eccesso di velocità",
+    plate: "EF 888 TY",
     paid: true
+  },
+  {
+    id: 703,
+    reason: "Mancato uso del casco",
+    plate: "EF 888 TY",
+    paid: false
+  },
+  {
+    id: 706,
+    reason: "Sosta vietata",
+    plate: "EF 888 TY",
+    paid: false
   }
 ];
 
 export default function App() {
   const fines = MOCK_FINES;
 
+  const [
+    paidFilter,
+    setPaidFilter
+  ] = React.useState("all");
+
   return (
     <div className="App">
       <h4>Le tue multe</h4>
 
       <div className="Filters">
-        <button>tutte</button>
-        <button>Solo Pagate</button>
-        <button>Solo non pagate</button>
+        <button
+          onClick={() =>
+            setPaidFilter("all")
+          }
+        >
+          Tutte
+        </button>
+        <button
+          onClick={() =>
+            setPaidFilter("only-paid")
+          }
+        >
+          Solo pagate
+        </button>
+        <button
+          onClick={() =>
+            setPaidFilter(
+              "only-not-paid"
+            )
+          }
+        >
+          Solo non pagate
+        </button>
       </div>
 
       <div className="Fines">
-        {/* COMMENTO MULTILINEA */}
-
-        {fines.map(fine => (
-          <FineCard
-            key={fine.id}
-            reason={fine.reason}
-            plate={fine.plate}
-            paid={fine.paid}
-          />
-        ))}
-
-        {/*   <FineCard
-          reason={MOCK_FINES[0].reason}
-          plate={MOCK_FINES[0].plate}
-          paid={MOCK_FINES[0].paid}
-        />
-        <FineCard plate="AD 456 CD" reason="Eccesso di velocità" />
-        <FineCard plate="AS 789 CD" reason="Mancato uso del casco" />
-
-        <div className="FineCard">
-          <h5>Gareggiamento</h5>
-          <p>
-            <strong> </strong>
-            <em />
-          </p>
-        </div>
-        <div className="FineCard paid">
-          <h5>Mancato uso del casco</h5>
-          <p>
-            <strong>Veicolo</strong>
-            {"  "}
-            <em>FB 123 CZ</em>
-          </p>
-        </div>
-       */}
+        {fines
+          .filter(fine => {
+            switch (paidFilter) {
+              case "all":
+                return true;
+              case "only-paid":
+                return fine.paid;
+              case "only-not-paid":
+                return !fine.paid;
+              default:
+                return true;
+            }
+          })
+          .map((fine, index) => (
+            <FineCard
+              key={index}
+              reason={fine.reason}
+              plate={fine.plate}
+              paid={fine.paid}
+            />
+          ))}
       </div>
     </div>
   );
 }
 
-function FineCard({ paid, reason, plate }) {
-  const [selected, setSelected] = React.useState(false); //False valore iniziale ìla presenza della parola use ci fa capire che è un HOOK
+const MOCK_LOCALES = [
+  {
+    languageId: "it_IT",
+    description: "Italian",
+    descriptionInLocale: "Italiano"
+  },
+  {
+    languageId: "en_US",
+    description: "English",
+    descriptionInLocale: "English"
+  },
+  {
+    languageId: "fr_FR",
+    description: "French",
+    descriptionInLocale: "François"
+  },
+  {
+    languageId: "de_DE",
+    description: "German",
+    descriptionInLocale:
+      "Dadieühdfaeiudh"
+  }
+];
+
+function MoveBox() {
+  // TARGET — left
+  // SOURCE — right
+
+  const [
+    targetIds,
+    setTargetIds
+  ] = React.useState([]);
+
+  const [
+    targetSelectedIds,
+    setTargetSelectedIds
+  ] = React.useState([]);
+
+  const [
+    sourceSelectedIds,
+    setSourceSelectedIds
+  ] = React.useState([]);
+
+  function toggleSourceId(id) {
+    setSourceSelectedIds(
+      toggleInArray(
+        sourceSelectedIds,
+        id
+      )
+    );
+  }
+
+  function toggleTargetId(id) {
+    setTargetSelectedIds(
+      toggleInArray(
+        targetSelectedIds,
+        id
+      )
+    );
+  }
+
+  function handleMoveToTargetClick() {
+    setSourceSelectedIds([]);
+    setTargetIds(
+      targetIds.concat(
+        sourceSelectedIds
+      )
+    );
+  }
+
+  function handleMoveToSourceClick() {
+    setTargetSelectedIds([]);
+    setTargetIds(
+      targetIds.filter(
+        id =>
+          !targetSelectedIds.includes(
+            id
+          )
+      )
+    );
+  }
+
+  return (
+    <div className="MoveBox">
+      <div className="MoveBoxPanel">
+        {MOCK_LOCALES.filter(locale =>
+          targetIds.includes(
+            locale.languageId
+          )
+        ).map(locale => (
+          <div
+            key={locale.languageId}
+            className={clsx(
+              "MoveBoxPanelItem",
+              targetSelectedIds.includes(
+                locale.languageId
+              ) && "selected"
+            )}
+            onClick={() => {
+              toggleTargetId(
+                locale.languageId
+              );
+            }}
+          >
+            {locale.description}
+          </div>
+        ))}
+      </div>
+      <div className="MoveBoxTrack">
+        <button
+          disabled={
+            sourceSelectedIds.length ===
+            0
+          }
+          onClick={
+            handleMoveToTargetClick
+          }
+        >
+          {"<"}
+        </button>
+        <button
+          disabled={
+            targetSelectedIds.length ===
+            0
+          }
+          onClick={
+            handleMoveToSourceClick
+          }
+        >
+          {">"}
+        </button>
+      </div>
+      <div className="MoveBoxPanel">
+        {MOCK_LOCALES.filter(
+          locale =>
+            !targetIds.includes(
+              locale.languageId
+            )
+        ).map(locale => (
+          <div
+            key={locale.languageId}
+            className={clsx(
+              "MoveBoxPanelItem",
+              sourceSelectedIds.includes(
+                locale.languageId
+              ) && "selected"
+            )}
+            onClick={() => {
+              toggleSourceId(
+                locale.languageId
+              );
+            }}
+          >
+            {locale.description}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function toggleInArray(array, item) {
+  if (array.includes(item)) {
+    return array.filter(
+      candidate => candidate !== item
+    );
+  } else {
+    return array.concat([item]);
+  }
+}
+
+function FineCard({
+  paid,
+  reason,
+  plate
+}) {
+  const [
+    selected,
+    setSelected
+  ] = React.useState(false);
+
+  const [
+    hover,
+    setHover
+  ] = React.useState(false);
+
   function handleClick(event) {
+    event.preventDefault();
     setSelected(!selected);
   }
+
+  function handleMouseEnter(event) {
+    setHover(true);
+  }
+
+  function handleMouseLeave(event) {
+    setHover(false);
+  }
+
   return (
     <div
-      className={clsx("FineCard", paid && "paid", selected && "selected")}
+      className={clsx(
+        "FineCard",
+        paid && "paid",
+        selected && "selected"
+      )}
       onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <h5>{reason}</h5>
       <p>
         <strong>Veicolo</strong>
-        {"  "}
-        <em>{plate}</em>
+      </p>
+      <p>
+        <code>
+          {hover ? plate : "** *** **"}
+        </code>
       </p>
     </div>
   );
